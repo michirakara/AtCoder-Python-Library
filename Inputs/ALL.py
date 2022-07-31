3c51940551f0fc566d4b4fc43d56269cdd8ff846
michirakara <101083390+michirakara@users.noreply.github.com>
2022-07-30 19:07:19 -0700

    Update ALL.py

--- Inputs/ALL.py
+++ Inputs/ALL.py
@@ -15,6 +15,10 @@ def Str():
 def ILH():
   return [*map(int,input().split())]
 
+#変数に代入するだけの時はこっちを使おう
+def ILHL():
+  return map(int,input().split())
+
 #一つの値ごとに改行されているn個の要素から成り立つ配列を受け取るよ
 def ILV(n):
   return [int(input()) for i in range(n)]
