76d633b9c5d5e9dde04ccb7aef62506e51904597
michirakara <101083390+michirakara@users.noreply.github.com>
2023-01-08 08:21:04 +0900

    Create IsPrime.py

---
+++ math/IsPrime.py
@@ -0,0 +1,26 @@
+import random
+
+
+def is_prime(n):
+    if n == 2:
+        return True
+    if n == 1 or n & 1 == 0:
+        return False
+
+    d = (n - 1) >> 1
+    while d & 1 == 0:
+        d >>= 1
+
+    for k in range(100):
+        a = random.randint(1, n - 1)
+        t = d
+        y = pow(a, t, n)
+
+        while t != n - 1 and y != 1 and y != n - 1:
+            y = (y * y) % n
+            t <<= 1
+
+        if y != n - 1 and t & 1 == 0:
+            return False
+
+    return True
