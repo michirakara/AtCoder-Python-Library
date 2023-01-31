5098a3f39b5490488b0adf656458117d8a98ef04
michirakara <101083390+michirakara@users.noreply.github.com>
2023-01-30 20:48:33 -0800

    Create BinaryIndexedTree.py

---
+++ DataStructure/Tree/BinaryIndexedTree.py
@@ -0,0 +1,25 @@
+class BIT:
+    # 0-indexed
+    def __init__(self, n):
+        self.size = n
+        self.tree = [0] * (n + 1)
+
+    def sum_(self, r):
+        if r == 0:
+            return 0
+        s = 0
+        r += 1
+        while r > 0:
+            s += self.tree[r]
+            r -= r & -r
+        return s
+
+    def sum(self, l, r):
+        # [l,r)
+        return self.sum_(r) - self.sum_(l)
+
+    def add(self, i, x):
+        i += 1
+        while i <= self.size:
+            self.tree[i] += x
+            i += i & -i
