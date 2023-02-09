c549e661e688d3642cadd3966c5c1667047bb6bb
michirakara <naotokonagai@gmail.com>
2023-02-08 16:50:54 -0800

    MultiSetのTreapを追加

---
+++ Tree/MultiTreap.py
@@ -0,0 +1,191 @@
+import random
+
+
+class TreapNode:
+    def __init__(self, data):
+        self.data = data  # ノードの値
+        self.priority = random.random()
+        self.left = None
+        self.right = None
+        self.count = 1
+        self.sum = data
+
+
+def count(t):
+    if not t:
+        return 0
+    return t.count
+
+
+def sum_(t):
+    if not t:
+        return 0
+    return t.sum
+
+
+def update(node):
+    node.count = count(node.left) + count(node.right) + 1
+    node.sum = sum_(node.left) + sum_(node.right) + node.data
+    return node
+
+
+def rotate_right(node):
+    lnode = node.left
+    node.left = lnode.right
+    lnode.right = node
+    update(node)
+    update(lnode)
+    return lnode
+
+
+def rotate_left(node):
+    rnode = node.right
+    node.right = rnode.left
+    rnode.left = node
+    update(node)
+    update(rnode)
+    return rnode
+
+
+def insert(node, x):
+    if node is None:
+        return TreapNode(x)
+    elif x < node.data or (x == node.data and random.random() < 0.5):
+        node.left = insert(node.left, x)
+        update(node)
+        if node.priority > node.left.priority:
+            node = rotate_right(node)
+    else:
+        node.right = insert(node.right, x)
+        update(node)
+        if node.priority > node.right.priority:
+            node = rotate_left(node)
+    return node
+
+
+def delete(node, x):
+    if node is not None:
+        if x == node.data:
+            if node.left is None and node.right is None:
+                return None
+            elif node.left is None:
+                node = rotate_left(node)
+                node.left = delete(node.left, x)
+            elif node.right is None:
+                node = rotate_right(node)
+                node.right = delete(node.right, x)
+            else:
+                if node.left.priority < node.right.priority:
+                    node = rotate_right(node)
+                    node.right = delete(node.right, x)
+                else:
+                    node = rotate_left(node)
+                    node.left = delete(node.left, x)
+            update(node)
+        elif x < node.data:
+            node.left = delete(node.left, x)
+            update(node)
+        else:
+            node.right = delete(node.right, x)
+            update(node)
+    return node
+
+
+def search(node, x):
+    ni = node.left.count if node.left else 0
+    while node is not None:
+        if x == node.data:
+            return ni
+        elif x < node.data:
+            node = node.left
+            if node is None:
+                return -1
+            ni -= node.right.count + 1 if node.right else 1
+        else:
+            node = node.right
+            if node is None:
+                return -1
+            ni += node.left.count + 1 if node.left else 1
+    return -1
+
+
+def traverse(node):
+    if node is not None:
+        for x in traverse(node.left):
+            yield x
+        yield node.data
+        for x in traverse(node.right):
+            yield x
+
+
+class MultiTreap:
+    def __init__(self, l=[]):
+        # 初期化したいリストを与えるとO(N log N)で初期化してくれます
+        self.root = None
+        if l:
+            for i in l:
+                self.insert(i)
+
+    def insert(self, x):
+        # xを挿入します O(logN)
+        self.root = insert(self.root, x)
+
+    def delete(self, x):
+        # xが存在する場合、xを削除します O(logN)
+        self.root = delete(self.root, x)
+
+    def sum(self, l, r):
+        # [l,r)の区間和を求めます
+        stack = [(self.root, self.root.left.count if self.root.left else 0)]
+        ret = 0
+        f = lambda n: n.count if n else 0
+        while stack:
+            look, ni = stack.pop()
+            if ni - f(look.left) >= l and ni + f(look.right) < r:
+                ret += look.sum
+            elif (
+                l > ni + f(look.right) >= ni - f(look.left)
+                or ni + f(look.right) >= ni - f(look.left) >= r
+            ):
+                pass
+            else:
+                if r > ni >= l:
+                    ret += look.data
+                if look.right:
+                    stack.append((look.right, ni + f(look.right.left) + 1))
+                if look.left:
+                    stack.append((look.left, ni - f(look.left.right) - 1))
+
+        return ret
+
+    def index(self, x):
+        # xが存在するならそのindexを、存在しないなら-1を返します O(logN)
+        return search(self.root, x)
+
+    def __contains__(self, x):
+        # xが存在するかを t in x で判定できます O(logN)
+        return search(self.root, x) != -1
+
+    def __getitem__(self, ind):
+        # t[index]で配列のindex番目に大きい要素にアクセスできます O(logN)
+        node = self.root
+        ni = node.left.count if node.left else 0
+        while ni != ind:
+            if ni > ind:
+                node = node.left
+                ni -= node.right.count + 1 if node.right else 1
+            else:
+                node = node.right
+                ni += node.left.count + 1 if node.left else 1
+        return node.data
+
+    def __str__(self):
+        # Treapをリスト形式で返します O(N)
+        if self.root is None:
+            return "Treap()"
+        buff = "Treap("
+        for x in traverse(self.root):
+            buff += "%s, " % x
+        buff = buff.rstrip(",  ")
+        buff += ")"
+        return buff
